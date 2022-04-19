e170cca6618dadefb4178b961002d727b601439a
seungbin9850 <tanja1206@naver.com>
2022-04-19 23:32:03 +0900

    easy-tuple_to_object

---
+++ easy/tuple_to_object/TupleToObject.ts
@@ -0,0 +1,5 @@
+type TupleToObject<T extends Readonly<any[]>> = { [P in T[number]]: P };
+
+const tuple = ["tesla", "model 3", "model X", "model Y"] as const;
+
+type result = TupleToObject<typeof tuple>; // expected { tesla: 'tesla', 'model 3': 'model 3', 'model X': 'model X', 'model Y': 'model Y'}
